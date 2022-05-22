120b53668f0d08a47f7e8b2bf886db7317301a03
obaidurx <mdobaidurbd@gmail.com>
2022-05-22 21:31:05 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -5,7 +5,27 @@ import Cards from "./components/Cards/Cards";
 import Filters from "./components/Filters/Filters";
 import Pagination from "./components/Pagination/Pagination";
 import Search from "./components/Search/Search";
+import Navbar from "./components/Navbar/Navbar";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Episodes from "./Pages/Episodes";
+import Location from "./Pages/Location";
 function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="episodes" element={<Episodes />} />
+        <Route path="location" element={<Location />} />
+      </Routes>
+    </Router>
+  );
+}
+
+const Home = () => {
   let [pageNumbers, setPageNumbers] = useState(1);
   let [search, setSearch] = useState("");
   let [status, setStatus] = useState(" ");
@@ -22,9 +42,6 @@ function App() {
   }, [api]);
   return (
     <div className="App">
-      <h1 className="text-center ubuntu my-4">
-        Rick & Morty <span className="text-primary">Wiki</span>
-      </h1>
       <Search setPageNumbers={setPageNumbers} setSearch={setSearch} />
       <div className="container">
         <div className="row">
@@ -49,6 +66,6 @@ function App() {
       />
     </div>
   );
-}
+};
 
 export default App;
