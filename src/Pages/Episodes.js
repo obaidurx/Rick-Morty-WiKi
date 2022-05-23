0799034519fb9b323aa107ba343030dd774ad1d2
obaidurx <mdobaidurbd@gmail.com>
2022-05-23 23:09:38 +0600

    Episode section is completed

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Cards from "../components/Cards/Cards";
+import InputGroup from "../components/Filters/Category/InputGroup";
 
 const Episodes = () => {
   let [id, setId] = useState(1);
@@ -32,7 +33,10 @@ const Episodes = () => {
         </h5>
       </div>
       <div className="row">
-        <div className="col-3">Pick Episodes</div>
+        <div className="col-3">
+          <h4 className="text-center mb-4"> Pick Episodes</h4>
+          <InputGroup setId={setId} name="Episode" total={51} />
+        </div>
         <div className="col-8">
           <div className="row">
             <Cards results={results} />
