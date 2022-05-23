431a5ce3c6b548fb08c47811fe7ce6315b0444b2
obaidurx <mdobaidurbd@gmail.com>
2022-05-23 11:37:04 +0600

    left of episode is completed

--- src/Pages/Episodes.js
+++ src/Pages/Episodes.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Cards from "../components/Cards/Cards";
 
 const Episodes = () => {
-  return <div>Episodes are here</div>;
+  let [id, setId] = useState(1);
+  let [info, setInfo] = useState([]);
+  let [results, setResults] = useState([]);
+  let { name, air_date } = info;
+  let api = `https://rickandmortyapi.com/api/episode/${id}`;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      setInfo(data);
+      let a = await Promise.all(
+        data.characters.map((x) => {
+          return fetch(x).then((res) => res.json());
+        })
+      );
+      setResults(a);
+    })();
+  }, [api]);
+  return (
+    <div className="container">
+      <div className="row mb-4">
+        <h1 className="text-center mb-4">
+          Episode :
+          <span className="text-primary"> {name === "" ? "Unknow" : name}</span>
+        </h1>
+        <h5 className="text-center">
+          Air Date {air_date === "" ? "Unknow" : air_date}
+        </h5>
+      </div>
+      <div className="row">
+        <div className="col-3">Pick Episodes</div>
+        <div className="col-8">
+          <div className="row">
+            <Cards results={results} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Episodes;
