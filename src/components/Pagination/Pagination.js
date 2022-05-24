bfa936c18af2ae10600dd8831f81746716ac4cbd
obaidurx <mdobaidurbd@gmail.com>
2022-05-24 20:24:03 +0600

    Alhamdulillah.Rick & Morty Wiki Project is done

--- src/components/Pagination/Pagination.js
+++ src/components/Pagination/Pagination.js
@@ -3,7 +3,6 @@ import ReactPaginate from "react-paginate";
 
 const Pagination = ({ info, setPageNumbers, pageNumbers }) => {
   let [width, setWidth] = useState(window.innerWidth);
-  console.log(width);
   let updateDimension = () => {
     setWidth(window.innerWidth);
   };
@@ -11,41 +10,47 @@ const Pagination = ({ info, setPageNumbers, pageNumbers }) => {
     window.addEventListener("resize", updateDimension);
     return () => window.removeEventListener("resize", updateDimension);
   }, []);
-  <style jsx>
-    {`
-      @media (max-width: 768px) {
-        .pagination {
-          font-size: 12px;
-        }
-        .next,
-        .prev {
-          display: none;
-        }
-      }
-      @media (max-width: 768px) {
-        .pagination {
-          font-size: 14px;
-        }
-      }
-    `}
-  </style>;
 
   return (
-    <ReactPaginate
-      className="pagination justify-content-center my-4 gap-4"
-      forcePage={pageNumbers === 1 ? 0 : pageNumbers - 1}
-      nextLabel="Next "
-      previousLabel="Prev"
-      nextClassName="btn btn-primary next"
-      previousClassName="btn btn-primary prev"
-      pageClassName="page-item"
-      pageLinkClassName="page-link"
-      activeClassName="active"
-      onPageChange={(data) => {
-        setPageNumbers(data.selected + 1);
-      }}
-      pageCount={info?.pages}
-    />
+    <>
+      <style jsx>
+        {`
+          @media (max-width: 768px) {
+            .pagination {
+              font-size: 12px;
+            }
+            .next,
+            .prev {
+              display: none;
+            }
+          }
+          @media (max-width: 768px) {
+            .pagination {
+              font-size: 14px;
+            }
+            .pagination {
+              font-size: 14px;
+            }
+          }
+        `}
+      </style>
+      <ReactPaginate
+        className="pagination justify-content-center my-4 gap-4"
+        forcePage={pageNumbers === 1 ? 0 : pageNumbers - 1}
+        nextLabel="Next "
+        previousLabel="Prev"
+        nextClassName="btn btn-primary next"
+        previousClassName="btn btn-primary prev"
+        pageClassName="page-item"
+        pageLinkClassName="page-link"
+        marginPagesDisplayed={width < 576 ? 1 : 2}
+        activeClassName="active"
+        onPageChange={(data) => {
+          setPageNumbers(data.selected + 1);
+        }}
+        pageCount={info?.pages}
+      />
+    </>
   );
 };
 export default Pagination;
